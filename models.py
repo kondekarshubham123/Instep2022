ad9e298eab3556ad76703ac7e41bfb584a781a3d
Shubham Kondekar <kondekarshubham123@gmail.com>
2022-07-24 12:34:45 +0530

    Dummy Drugs data Added

--- models.py
+++ models.py
@@ -16,4 +16,16 @@ class Drugs(db.Model):
     drug_indication = db.Column(db.String(100))
     drug_details = db.Column(db.String(1000))
     drug_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    drug_process = db.Column(db.Integer, default=randint(0,100))
+    drug_process = db.Column(db.Integer)
+
+    def __init__(self,drug_name,drug_indication,drug_details,drug_process) -> None:
+        self.drug_name = drug_name
+        self.drug_indication = drug_indication
+        self.drug_details = drug_details
+        self.drug_process = drug_process
+
+    @staticmethod
+    def create(drug_name,drug_indication,drug_details):
+        new_drug = Drugs(drug_name,drug_indication,drug_details,randint(0,100))
+        db.session.add(new_drug)
+        db.session.commit()
